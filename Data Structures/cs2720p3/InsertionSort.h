742658ea8fa9fc2b2ff41947de31b699d704cdf5
derekfreed <derekf@uga.edu>
2015-02-02 16:54:50 -0500

    website fix

--- Data Structures/cs2720p3/InsertionSort.h
+++
@@ -1,6 +0,0 @@
-//Derek Freed
-//CSCI 2720
-//InsertionSort.h
-
-void insertionSort(int [], int);
-long getInsertionComparison();
--- Data Structures/cs2720p3/MergeSort.h
+++
@@ -1,7 +0,0 @@
-//Derek Freed
-//CSCI 2720
-//MergeSort.h
-
-void merge(int [], int, int, int);
-void mergeSort(int [], int, int);
-long getMergeComparison();
